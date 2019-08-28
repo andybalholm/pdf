b9ae0af6932e35f6e3fe49993b915acf6767f45d
Andy Balholm <andy@balholm.com>
2019-08-28 14:48:16 -0700

    Start with my experiment of embedding Type 3 font subsets.

--- font.go
+++ font.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"sort"
 	"strings"
 
 	"golang.org/x/image/font"
@@ -90,11 +92,12 @@ func (s sortedRunes) Less(i, j int) bool { return s[i] < s[j] }
 func (s sortedRunes) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func (f *Font) writeTo(e *encoder) {
-	var buffer sfnt.Buffer
-
 	firstChar, lastChar := 32, 255
 	widths := make([]int, lastChar-firstChar+1)
+	cp := &charProcs{procs: make(map[string]*type3Glyph)}
+
 	for i := firstChar; i <= lastChar; i++ {
+		var buffer sfnt.Buffer
 		r := charmap.Windows1252.DecodeByte(byte(i))
 		g, err := f.sfnt.GlyphIndex(&buffer, r)
 		if err != nil {
@@ -105,50 +108,129 @@ func (f *Font) writeTo(e *encoder) {
 			continue
 		}
 		widths[i-firstChar] = w.Round()
+		name, err := f.sfnt.GlyphName(&buffer, g)
+		if err != nil {
+			continue
+		}
+		if name == "" {
+			name = glyphName(r)
+		}
+		outlines, err := f.sfnt.LoadGlyph(&buffer, g, fixed.I(1000), nil)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		cp.procs[name] = &type3Glyph{
+			outline: outlines,
+			width:   w.Round(),
+		}
 	}
-
-	fmt.Fprintf(e, "<< /Type /Font /Subtype /%v /BaseFont /%s /Encoding /WinAnsiEncoding\n", f.typ, f.name)
-	fmt.Fprintf(e, "/FontDescriptor %d 0 R\n", e.getRef(&fontDescriptor{f}))
-	fmt.Fprintf(e, "/FirstChar %d /LastChar %d\n", firstChar, lastChar)
-	fmt.Fprintf(e, "/Widths %d\n", widths)
-	fmt.Fprint(e, ">>")
-}
-
-type fontDescriptor struct {
-	f *Font
-}
-
-func (f *fontDescriptor) writeTo(e *encoder) {
-	fontFile := &stream{}
-	fontFile.enableFlate()
-	fontFile.Write(f.f.data)
-	fontFile.extraData = fmt.Sprintf("/Subtype /%v", f.f.typ)
 
 	var buffer sfnt.Buffer
 
 	var bounds []int
-	rawBounds, err := f.f.sfnt.Bounds(&buffer, fixed.I(1000), font.HintingNone)
+	rawBounds, err := f.sfnt.Bounds(&buffer, fixed.I(1000), font.HintingNone)
 	if err == nil {
 		bounds = []int{rawBounds.Min.X.Floor(), -rawBounds.Max.Y.Ceil(), rawBounds.Max.X.Ceil(), -rawBounds.Min.Y.Floor()}
 	}
 
-	var italicAngle float64
-	if pt := f.f.sfnt.PostTable(); pt != nil {
-		italicAngle = pt.ItalicAngle
+	fmt.Fprintf(e, "<< /Type /Font /Subtype /Type3 /Encoding /WinAnsiEncoding\n")
+	fmt.Fprintf(e, "/FontBBox %d\n", bounds)
+	fmt.Fprintf(e, "/FontMatrix [0.001 0 0 0.001 0 0]\n")
+	fmt.Fprintf(e, "/FirstChar %d /LastChar %d\n", firstChar, lastChar)
+	fmt.Fprintf(e, "/Widths %d\n", widths)
+	fmt.Fprintf(e, "/CharProcs %d 0 R\n", e.getRef(cp))
+	fmt.Fprint(e, ">>")
+}
+
+type type3Glyph struct {
+	outline []sfnt.Segment
+	width   int
+}
+
+func (g *type3Glyph) writeTo(e *encoder) {
+	var min, max fixed.Point26_6
+	if len(g.outline) > 0 {
+		min = g.outline[0].Args[0]
+		max = min
+	}
+	for _, segment := range g.outline {
+		pointCount := int(segment.Op)
+		if pointCount == 0 {
+			pointCount = 1
+		}
+		for i := 0; i < pointCount; i++ {
+			p := segment.Args[i]
+			if p.X < min.X {
+				min.X = p.X
+			}
+			if p.Y < min.Y {
+				min.Y = p.Y
+			}
+			if p.X > max.X {
+				max.X = p.X
+			}
+			if p.Y > max.Y {
+				max.Y = p.Y
+			}
+		}
+	}
+	min.Y, max.Y = -max.Y, -min.Y
+
+	s := new(stream)
+	s.enableFlate()
+	fmt.Fprintf(s, "%d 0 %d %d %d %d d1\n", g.width, min.X.Floor(), min.Y.Floor(), max.X.Ceil(), max.Y.Ceil())
+	var current fixed.Point26_6
+	for _, segment := range g.outline {
+		switch segment.Op {
+		case sfnt.SegmentOpMoveTo:
+			fmt.Fprintf(s, "%d %d m\n", segment.Args[0].X.Round(), -segment.Args[0].Y.Round())
+			current = segment.Args[0]
+		case sfnt.SegmentOpLineTo:
+			fmt.Fprintf(s, "%d %d l\n", segment.Args[0].X.Round(), -segment.Args[0].Y.Round())
+			current = segment.Args[0]
+		case sfnt.SegmentOpQuadTo:
+			args := segment.Args
+			control1 := fixed.Point26_6{
+				X: current.X + (args[0].X-current.X)*2/3,
+				Y: current.Y + (args[0].Y-current.Y)*2/3,
+			}
+			control2 := fixed.Point26_6{
+				X: args[1].X + (args[0].X-args[1].X)*2/3,
+				Y: args[1].Y + (args[0].Y-args[1].Y)*2/3,
+			}
+			fmt.Fprintf(s, "%d %d %d %d %d %d c\n", control1.X.Round(), -control1.Y.Round(), control2.X.Round(), -control2.Y.Round(), args[1].X.Round(), -args[1].Y.Round())
+			current = args[1]
+		case sfnt.SegmentOpCubeTo:
+			for _, p := range segment.Args {
+				fmt.Fprintf(s, "%d %d ", p.X.Round(), -p.Y.Round())
+			}
+			fmt.Fprintln(s, "c")
+			current = segment.Args[2]
+		}
+	}
+	if len(g.outline) > 0 {
+		fmt.Fprint(s, "f")
 	}
 
-	metrics, _ := f.f.sfnt.Metrics(&buffer, fixed.I(1000), font.HintingNone)
+	s.writeTo(e)
+}
 
-	fmt.Fprintln(e, "<< /Type /FontDescriptor")
-	fmt.Fprintf(e, "/FontName /%s\n", f.f.name)
-	fmt.Fprintf(e, "/Flags 4\n")
-	fmt.Fprintf(e, "/FontBBox %d\n", bounds)
-	fmt.Fprintf(e, "/ItalicAngle %g\n", italicAngle)
-	fmt.Fprintf(e, "/Ascent %d\n", metrics.Ascent.Round())
-	fmt.Fprintf(e, "/Descent %d\n", metrics.Descent.Round())
-	fmt.Fprintf(e, "/CapHeight %d\n", metrics.CapHeight.Round())
-	fmt.Fprintf(e, "/StemV 80 /StemH 80\n")
-	fmt.Fprintf(e, "/FontFile3 %d 0 R\n", e.getRef(fontFile))
+type charProcs struct {
+	procs map[string]*type3Glyph
+}
+
+func (c *charProcs) writeTo(e *encoder) {
+	names := make([]string, 0, len(c.procs))
+	for name := range c.procs {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+
+	fmt.Fprintln(e, "<<")
+	for _, name := range names {
+		fmt.Fprintf(e, "/%s %d 0 R\n", name, e.getRef(c.procs[name]))
+	}
 	fmt.Fprint(e, ">>")
 }
 
