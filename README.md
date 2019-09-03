3f1ec5b7ae4434b46c294c3481bc17eb974ead5b
Andy Balholm <andy@balholm.com>
2019-09-03 09:12:28 -0700

    Revert "Go back to embedding the whole font file."
    The Type 3 fonts weren't the problem after all. But I still don't
    know what was.
    
    This reverts commit 6b2e9a54a8d93ca5d1cd6bc1ab156a7849e3e0fd.

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ The most interesting feature is its font support.
 ## Font Features
 
 - Supports both TrueType and OpenType fonts.
-- All text is UTF-8; supports any Unicode character.
+- All text is UTF-8; supports any Unicode character (but only 255 per font).
+- Embeds font subsets, converted to Type 3 outline fonts.
+
+## Why Type 3?
+
+PDF Type 3 fonts have a bad reputation, because they are often used for bitmap fonts.
+But it's actually a pretty nice format.
+The only real disadvantage is that it doesn't have hinting.
+I like the fact that it's PDF objects all the way down—no binary blobs.
--- font.go
+++ font.go
@@ -1,33 +1,24 @@
 package pdf
 
 import (
-	"encoding/binary"
-	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"sort"
 	"strconv"
 	"strings"
-	"unicode/utf16"
 
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/sfnt"
 	"golang.org/x/image/math/fixed"
-)
-
-type fontType int
-
-const (
-	trueType fontType = iota
-	openType
+	"golang.org/x/text/encoding/charmap"
 )
 
 type Font struct {
-	typ        fontType
-	name       string
-	data       []byte
-	sfnt       *sfnt.Font
-	usedGlyphs map[rune]sfnt.GlyphIndex
+	sfnt *sfnt.Font
+
+	encode    map[rune]byte
+	toUnicode [256]rune
 }
 
 // LoadFont loads a TrueType or OpenType font from the file specified. If it
@@ -44,30 +35,12 @@ func (d *Document) LoadFont(filename string) (*Font, error) {
 	}
 
 	f := &Font{
-		data:       b,
-		usedGlyphs: make(map[rune]sfnt.GlyphIndex),
-	}
-
-	if len(b) < 4 {
-		return nil, errors.New("font file too small")
-	}
-	switch string(b[:4]) {
-	case "true", "\x00\x01\x00\x00":
-		f.typ = trueType
-	case "OTTO":
-		f.typ = openType
-	default:
-		return nil, errors.New("unrecognized font format")
+		encode: make(map[rune]byte),
 	}
 
 	f.sfnt, err = sfnt.Parse(b)
 	if err != nil {
 		return nil, err
-	}
-
-	f.name, err = f.sfnt.Name(nil, sfnt.NameIDPostScript)
-	if err != nil {
-		return nil, errors.New("missing PostScript font name")
 	}
 
 	if d.fontCache == nil {
@@ -77,144 +50,169 @@ func (d *Document) LoadFont(filename string) (*Font, error) {
 	return f, nil
 }
 
-type sortedRunes []rune
-
-func (s sortedRunes) Len() int           { return len(s) }
-func (s sortedRunes) Less(i, j int) bool { return s[i] < s[j] }
-func (s sortedRunes) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-
 func (f *Font) writeTo(e *encoder) {
-	var CIDType int
-	switch f.typ {
-	case trueType:
-		CIDType = 2
-	case openType:
-		CIDType = 0
-	}
-
-	var buffer sfnt.Buffer
-	numGlyphs := f.sfnt.NumGlyphs()
-	widths := make([]int, numGlyphs)
-	for i := range widths {
-		w, err := f.sfnt.GlyphAdvance(&buffer, sfnt.GlyphIndex(i), fixed.I(1000), font.HintingNone)
-		if err == nil {
-			widths[i] = w.Round()
+	var firstChar, lastChar int
+	for i := 0; i < 256; i++ {
+		if f.toUnicode[i] != 0 {
+			firstChar = i
+			break
 		}
 	}
-
-	// Build the ToUnicode CMap.
-	runes := make(sortedRunes, 0, len(f.usedGlyphs))
-	for r := range f.usedGlyphs {
-		runes = append(runes, r)
+	for i := 255; i >= firstChar; i-- {
+		if f.toUnicode[i] != 0 {
+			lastChar = i
+			break
+		}
 	}
-	sort.Sort(runes)
+	widths := make([]int, lastChar-firstChar+1)
+	cp := &charProcs{procs: make(map[string]*type3Glyph)}
+	var differences []string
+	prevDifference := -1
 
-	tu := new(stream)
-	fmt.Fprintln(tu, "/CIDInit /ProcSet findresource begin")
-	fmt.Fprintln(tu, "12 dict begin")
-	fmt.Fprintln(tu, "begincmap")
-	fmt.Fprintln(tu, "/CIDSystemInfo << /Registry (Adobe)\n/Ordering (UCS)\n/Supplement 0 >> def")
-	fmt.Fprintln(tu, "/CMapName /Adobe-Identity-UCS def")
-	fmt.Fprintln(tu, "/CMapType 2 def")
-	fmt.Fprintln(tu, "1 begincodespacerange")
-	fmt.Fprintln(tu, "<0000> <FFFF>")
-	fmt.Fprintln(tu, "endcodespacerange")
-	for i, r := range runes {
-		if i%100 == 0 {
-			if i != 0 {
-				fmt.Fprintln(tu, "endbfchar")
+	for i := firstChar; i <= lastChar; i++ {
+		var buffer sfnt.Buffer
+		r := f.toUnicode[i]
+		if r == 0 {
+			continue
+		}
+		name := glyphName(r)
+		if r != charmap.Windows1252.DecodeByte(byte(i)) {
+			if prevDifference != i-1 {
+				differences = append(differences, fmt.Sprint(i))
 			}
-			if len(runes) < i+100 {
-				fmt.Fprintf(tu, "%d beginbfchar\n", len(runes)-i)
-			} else {
-				fmt.Fprintln(tu, "100 beginbfchar")
-			}
+			differences = append(differences, "/"+name)
+			prevDifference = i
 		}
-		if r < 0x10000 {
-			fmt.Fprintf(tu, "<%04X> <%04X>\n", f.usedGlyphs[r], r)
-		} else {
-			r1, r2 := utf16.EncodeRune(r)
-			fmt.Fprintf(tu, "<%04X> <%04X%04X>\n", f.usedGlyphs[r], r1, r2)
+
+		g, err := f.sfnt.GlyphIndex(&buffer, r)
+		if err != nil {
+			continue
 		}
-	}
-	fmt.Fprintln(tu, "endbfchar")
-	fmt.Fprintln(tu, "endcmap")
-	fmt.Fprintln(tu, "CMapName currentdict /CMap defineresource pop")
-	fmt.Fprintln(tu, "end\nend")
-
-	fmt.Fprintf(e, "<< /Type /Font /Subtype /Type0 /BaseFont /%s /Encoding /Identity-H /ToUnicode %d 0 R\n", f.name, e.getRef(tu))
-	fmt.Fprintf(e, "/DescendantFonts [ << /Type /Font /Subtype /CIDFontType%d /BaseFont /%s /CIDToGIDMap /Identity\n", CIDType, f.name)
-
-	fmt.Fprintf(e, "/DW %d ", widths[0])
-	fmt.Fprintf(e, "/W [0 %d]\n", widths)
-
-	fmt.Fprintln(e, "/CIDSystemInfo << /Registry (Adobe) /Ordering (Identity) /Supplement 0 >>")
-	fmt.Fprintf(e, "/FontDescriptor %d 0 R\n", e.getRef(&fontDescriptor{f}))
-	fmt.Fprint(e, ">> ] >>")
-}
-
-func (f *Font) toGlyph(r rune) sfnt.GlyphIndex {
-	var buffer sfnt.Buffer
-	gi, ok := f.usedGlyphs[r]
-	if ok {
-		return gi
-	}
-	gi, err := f.sfnt.GlyphIndex(&buffer, r)
-	if err != nil {
-		return 0
-	}
-	f.usedGlyphs[r] = gi
-	return gi
-}
-
-func (f *Font) toGlyphs(s string) []sfnt.GlyphIndex {
-	glyphs := make([]sfnt.GlyphIndex, 0, len(s))
-	for _, r := range s {
-		glyphs = append(glyphs, f.toGlyph(r))
-	}
-	return glyphs
-}
-
-type fontDescriptor struct {
-	f *Font
-}
-
-func (f *fontDescriptor) writeTo(e *encoder) {
-	fontFile := &stream{}
-	fontFile.enableFlate()
-	fontFile.Write(f.f.data)
-	switch f.f.typ {
-	case trueType:
-		fontFile.extraData = "/Subtype /TrueType"
-	case openType:
-		fontFile.extraData = "/Subtype /OpenType"
+		w, err := f.sfnt.GlyphAdvance(&buffer, g, fixed.I(1000), font.HintingNone)
+		if err != nil {
+			continue
+		}
+		widths[i-firstChar] = w.Round()
+		outlines, err := f.sfnt.LoadGlyph(&buffer, g, fixed.I(1000), nil)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		cp.procs[name] = &type3Glyph{
+			outline: outlines,
+			width:   w.Round(),
+		}
 	}
 
 	var buffer sfnt.Buffer
 
 	var bounds []int
-	rawBounds, err := f.f.sfnt.Bounds(&buffer, fixed.I(1000), font.HintingNone)
+	rawBounds, err := f.sfnt.Bounds(&buffer, fixed.I(1000), font.HintingNone)
 	if err == nil {
 		bounds = []int{rawBounds.Min.X.Floor(), -rawBounds.Max.Y.Ceil(), rawBounds.Max.X.Ceil(), -rawBounds.Min.Y.Floor()}
 	}
 
-	var italicAngle float64
-	if pt := f.f.sfnt.PostTable(); pt != nil {
-		italicAngle = pt.ItalicAngle
+	fmt.Fprintf(e, "<< /Type /Font /Subtype /Type3\n")
+	if len(differences) == 0 {
+		fmt.Fprintln(e, "/Encoding /WinAnsiEncoding")
+	} else {
+		fmt.Fprintf(e, "/Encoding << /BaseEncoding /WinAnsiEncoding /Differences %v >>\n", differences)
+	}
+	fmt.Fprintf(e, "/FontBBox %d\n", bounds)
+	fmt.Fprintf(e, "/FontMatrix [0.001 0 0 0.001 0 0]\n")
+	fmt.Fprintf(e, "/FirstChar %d /LastChar %d\n", firstChar, lastChar)
+	fmt.Fprintf(e, "/Widths %d\n", widths)
+	fmt.Fprintf(e, "/CharProcs %d 0 R\n", e.getRef(cp))
+	fmt.Fprint(e, ">>")
+}
+
+type type3Glyph struct {
+	outline []sfnt.Segment
+	width   int
+}
+
+func (g *type3Glyph) writeTo(e *encoder) {
+	var min, max fixed.Point26_6
+	if len(g.outline) > 0 {
+		min = g.outline[0].Args[0]
+		max = min
+	}
+	for _, segment := range g.outline {
+		pointCount := int(segment.Op)
+		if pointCount == 0 {
+			pointCount = 1
+		}
+		for i := 0; i < pointCount; i++ {
+			p := segment.Args[i]
+			if p.X < min.X {
+				min.X = p.X
+			}
+			if p.Y < min.Y {
+				min.Y = p.Y
+			}
+			if p.X > max.X {
+				max.X = p.X
+			}
+			if p.Y > max.Y {
+				max.Y = p.Y
+			}
+		}
+	}
+	min.Y, max.Y = -max.Y, -min.Y
+
+	s := new(stream)
+	fmt.Fprintf(s, "%d 0 %d %d %d %d d1\n", g.width, min.X.Floor(), min.Y.Floor(), max.X.Ceil(), max.Y.Ceil())
+	var current fixed.Point26_6
+	for _, segment := range g.outline {
+		switch segment.Op {
+		case sfnt.SegmentOpMoveTo:
+			fmt.Fprintf(s, "%d %d m\n", segment.Args[0].X.Round(), -segment.Args[0].Y.Round())
+			current = segment.Args[0]
+		case sfnt.SegmentOpLineTo:
+			fmt.Fprintf(s, "%d %d l\n", segment.Args[0].X.Round(), -segment.Args[0].Y.Round())
+			current = segment.Args[0]
+		case sfnt.SegmentOpQuadTo:
+			args := segment.Args
+			control1 := fixed.Point26_6{
+				X: current.X + (args[0].X-current.X)*2/3,
+				Y: current.Y + (args[0].Y-current.Y)*2/3,
+			}
+			control2 := fixed.Point26_6{
+				X: args[1].X + (args[0].X-args[1].X)*2/3,
+				Y: args[1].Y + (args[0].Y-args[1].Y)*2/3,
+			}
+			fmt.Fprintf(s, "%d %d %d %d %d %d c\n", control1.X.Round(), -control1.Y.Round(), control2.X.Round(), -control2.Y.Round(), args[1].X.Round(), -args[1].Y.Round())
+			current = args[1]
+		case sfnt.SegmentOpCubeTo:
+			for _, p := range segment.Args {
+				fmt.Fprintf(s, "%d %d ", p.X.Round(), -p.Y.Round())
+			}
+			fmt.Fprintln(s, "c")
+			current = segment.Args[2]
+		}
+	}
+	if len(g.outline) > 0 {
+		fmt.Fprint(s, "f")
 	}
 
-	metrics, _ := f.f.sfnt.Metrics(&buffer, fixed.I(1000), font.HintingNone)
+	s.writeTo(e)
+}
 
-	fmt.Fprintln(e, "<< /Type /FontDescriptor")
-	fmt.Fprintf(e, "/FontName /%s\n", f.f.name)
-	fmt.Fprintf(e, "/Flags 4\n")
-	fmt.Fprintf(e, "/FontBBox %d\n", bounds)
-	fmt.Fprintf(e, "/ItalicAngle %g\n", italicAngle)
-	fmt.Fprintf(e, "/Ascent %d\n", metrics.Ascent.Round())
-	fmt.Fprintf(e, "/Descent %d\n", metrics.Descent.Round())
-	fmt.Fprintf(e, "/CapHeight %d\n", metrics.CapHeight.Round())
-	fmt.Fprintf(e, "/StemV 80 /StemH 80\n")
-	fmt.Fprintf(e, "/FontFile3 %d 0 R\n", e.getRef(fontFile))
+type charProcs struct {
+	procs map[string]*type3Glyph
+}
+
+func (c *charProcs) writeTo(e *encoder) {
+	names := make([]string, 0, len(c.procs))
+	for name := range c.procs {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+
+	fmt.Fprintln(e, "<<")
+	for _, name := range names {
+		fmt.Fprintf(e, "/%s %d 0 R\n", name, e.getRef(c.procs[name]))
+	}
 	fmt.Fprint(e, ">>")
 }
 
@@ -238,6 +236,52 @@ func (p *Page) SetLeading(leading float64) {
 	fmt.Fprintf(p.contents, "%g TL ", leading)
 }
 
+func (f *Font) encodeRune(r rune) (b byte, ok bool) {
+	if b, ok := f.encode[r]; ok {
+		return b, true
+	}
+	if b, ok := charmap.Windows1252.EncodeRune(r); ok && f.toUnicode[b] == 0 {
+		f.encode[r] = b
+		f.toUnicode[b] = r
+		return b, true
+	}
+
+	for i := 31; i > 0 && b == 0; i-- {
+		if f.toUnicode[i] == 0 {
+			b = byte(i)
+		}
+	}
+	for i := 127; i < 256 && b == 0; i++ {
+		if f.toUnicode[i] == 0 {
+			b = byte(i)
+		}
+	}
+	for i := 126; i > 32 && b == 1; i-- {
+		if f.toUnicode[i] == 0 {
+			b = byte(i)
+		}
+	}
+
+	if b != 0 {
+		f.encode[r] = b
+		f.toUnicode[b] = r
+		return b, true
+	}
+
+	return 0, false
+}
+
+func (f *Font) encodeString(s string) string {
+	b := make([]byte, 0, len(s))
+	for _, r := range s {
+		c, ok := f.encodeRune(r)
+		if ok {
+			b = append(b, c)
+		}
+	}
+	return string(b)
+}
+
 func (f *Font) runeWidth(r rune) int {
 	var buffer sfnt.Buffer
 	g, err := f.sfnt.GlyphIndex(&buffer, r)
@@ -256,33 +300,39 @@ func (f *Font) runeWidth(r rune) int {
 // in units of 1/1000 of an em. If maxWidth is nonzero and s is too long to fit,
 // the result will be truncated.
 func (f *Font) encodeAndKern(s string, maxWidth int) (tj []string, width int) {
-	glyphs := f.toGlyphs(s)
+	s = f.encodeString(s)
 	var buffer sfnt.Buffer
 
+	var prevGlyph sfnt.GlyphIndex
 	chunkStart := 0
-	for i, g := range glyphs {
+	for i := 0; i < len(s); i++ {
+		g, err := f.sfnt.GlyphIndex(&buffer, f.toUnicode[s[i]])
+		if err != nil {
+			continue
+		}
 		advance, err := f.sfnt.GlyphAdvance(&buffer, g, fixed.I(1000), font.HintingNone)
 		if err == nil {
 			oldWidth := width
 			width += advance.Round()
 			if maxWidth != 0 && width > maxWidth {
-				tj = append(tj, quoteGlyphIndices(glyphs[chunkStart:i]))
+				tj = append(tj, quoteString(s[chunkStart:i]))
 				return tj, oldWidth
 			}
 		}
 		if i != 0 {
-			kern, err := f.sfnt.Kern(&buffer, glyphs[i-1], g, fixed.I(1000), font.HintingNone)
+			kern, err := f.sfnt.Kern(&buffer, prevGlyph, g, fixed.I(1000), font.HintingNone)
 			if err == nil && kern != 0 {
 				width += kern.Round()
 				tj = append(tj,
-					quoteGlyphIndices(glyphs[chunkStart:i]),
+					quoteString(s[chunkStart:i]),
 					strconv.Itoa(-kern.Round()),
 				)
 				chunkStart = i
 			}
 		}
+		prevGlyph = g
 	}
-	tj = append(tj, quoteGlyphIndices(glyphs[chunkStart:]))
+	tj = append(tj, quoteString(s[chunkStart:]))
 
 	return tj, width
 }
@@ -291,12 +341,6 @@ var stringEscaper = strings.NewReplacer("\n", `\n`, "\r", `\r`, "\t", `\t`, "(",
 
 func quoteString(s string) string {
 	return "(" + stringEscaper.Replace(s) + ")"
-}
-
-func quoteGlyphIndices(glyphs []sfnt.GlyphIndex) string {
-	b := new(strings.Builder)
-	binary.Write(b, binary.BigEndian, glyphs)
-	return quoteString(b.String())
 }
 
 // beginText begins a text object. All text output and positioning must happen
@@ -364,9 +408,11 @@ func (p *Page) Truncate(x, y, width float64, s string) {
 		p.endText()
 		return
 	}
-	ellipsis, ellipsisWidth := p.currentFont.encodeAndKern("…", 0)
-	tj, _ := p.currentFont.encodeAndKern(s, scaledWidth-ellipsisWidth)
-	tj = append(tj, ellipsis...)
+	tj, _ := p.currentFont.encodeAndKern(s, scaledWidth-p.currentFont.runeWidth('…'))
+	ellipsis, ok := p.currentFont.encodeRune('…')
+	if ok {
+		tj = append(tj, quoteString(string([]byte{ellipsis})))
+	}
 	fmt.Fprintf(p.contents, "%v TJ ", tj)
 	p.endText()
 }
