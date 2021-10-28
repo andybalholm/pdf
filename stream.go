176f3ca7b5150421ff649a7e5d70309da035f1e5
Andy Balholm <andy@balholm.com>
2021-10-28 09:27:47 -0700

    Turn on flate compression of streams by default.

--- stream.go
+++ stream.go
@@ -7,36 +7,41 @@ import (
 )
 
 type stream struct {
-	b  bytes.Buffer
-	fw *zlib.Writer
+	b bytes.Buffer
 
 	extraData string
 }
 
 func (s *stream) Write(p []byte) (n int, err error) {
-	if s.fw != nil {
-		return s.fw.Write(p)
-	}
 	return s.b.Write(p)
 }
 
 func (s *stream) writeTo(e *encoder) {
-	if s.fw != nil {
-		s.fw.Close()
+	compressed := false
+	cb := new(bytes.Buffer)
+	zw := zlib.NewWriter(cb)
+	if _, err := zw.Write(s.b.Bytes()); err == nil {
+		if err := zw.Close(); err == nil {
+			if cb.Len() < s.b.Len()-len("/Filter /FlateDecode ") {
+				compressed = true
+			}
+		}
 	}
-	fmt.Fprintf(e, "<< /Length %d ", s.b.Len())
-	if s.fw != nil {
-		fmt.Fprint(e, "/Filter /FlateDecode ")
+
+	if compressed {
+		fmt.Fprintf(e, "<< /Length %d /Filter /FlateDecode ", cb.Len())
+	} else {
+		fmt.Fprintf(e, "<< /Length %d ", s.b.Len())
 	}
 	if s.extraData != "" {
 		fmt.Fprint(e, s.extraData, " ")
 	}
 	fmt.Fprintln(e, ">>")
 	e.WriteString("stream\n")
-	e.Write(s.b.Bytes())
+	if compressed {
+		e.Write(cb.Bytes())
+	} else {
+		e.Write(s.b.Bytes())
+	}
 	e.WriteString("\nendstream")
-}
-
-func (s *stream) enableFlate() {
-	s.fw = zlib.NewWriter(&s.b)
 }
